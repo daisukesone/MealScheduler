fe772b246f3b3879437c35efd362495f2fcda52d
daisukesone <sonedai0213@gmail.com>
2018-03-04 16:02:37 +0900

    RSS実装

---
+++ MealScheduler/DetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewController.swift
+//  MealScheduler
+//
+//  Created by 曽根大輔 on 2018/03/04.
+//  Copyright © 2018年 曽根大輔. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController : UIViewController  {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var link: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = URL(string: self.link) {
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }
+    }
+}
---
+++ MealScheduler/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  MealScheduler
+//
+//  Created by 曽根大輔 on 2018/03/04.
+//  Copyright © 2018年 曽根大輔. All rights reserved.
+//
+
+import Foundation
+
+
+class  Item {
+    
+    var title = ""
+    var link = ""
+}
+
